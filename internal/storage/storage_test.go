0782693f9a30cb47cb24e756ea5974a5d9a7ff29
vili-ping <drdlemmer@gmail.com>
2025-03-30 20:55:38 +0300

    Add tests for new handlers

--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -57,3 +57,79 @@ func TestMemStorage_SetMetric(t *testing.T) {
 		})
 	}
 }
+
+func TestMemStorage_GetAllMetrics(t *testing.T) {
+	type fields struct {
+		Vals map[string]string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name:   "Get all metrics in storage",
+			fields: fields{Vals: map[string]string{"Alloc": "123"}},
+			want:   "Alloc=123\n",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := MemStorage{
+				Vals: tt.fields.Vals,
+			}
+
+			result := m.GetAllMetrics()
+
+			assert.Equal(t, tt.want, result, "Metrics convert string not right: exp: %s, get %s", tt.want, result)
+		})
+	}
+}
+
+func TestMemStorage_GetMetric(t *testing.T) {
+	type fields struct {
+		Vals map[string]string
+	}
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "Get metric by Alloc name",
+			fields:  fields{Vals: map[string]string{"Alloc": "123"}},
+			args:    args{key: "Alloc"},
+			want:    "123",
+			wantErr: false,
+		},
+		{
+			name:    "Get eror when key is not exist",
+			fields:  fields{Vals: map[string]string{"Alloc": "123"}},
+			args:    args{key: "HeartKey"},
+			want:    "123",
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := MemStorage{
+				Vals: tt.fields.Vals,
+			}
+			got, err := m.GetMetric(tt.args.key)
+
+			if tt.wantErr {
+				assert.EqualError(t, err, "storage key is not exists", "eror is wrong")
+				return
+			}
+
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
